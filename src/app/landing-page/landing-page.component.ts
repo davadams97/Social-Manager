22d44e38877a87da106e4220a53605e65967ed3c
David A. Adams <david.adams97@gmail.com>
2020-07-09 18:04:33 -0300

    feat(landing-page): load services

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
 import {LandingPageService} from './landing-page.service';
+
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, OnDestroy {
+  private _destroy$: Subject<any>;
 
-  constructor(private _lpService: LandingPageService) { }
+  constructor(private _lpService: LandingPageService) {
+  }
 
-  ngOnInit() {
-    this._lpService.getExpiringPerCountry().subscribe(res => console.log(res));
+  public ngOnInit() {
+    this._load();
+  }
+
+  public ngOnDestroy() {
+    this._destroy$.next();
+  }
+
+  private _load(): void {
+    this._lpService.getExpiringPerCountry$('CA').subscribe((res) => {
+      const keys = res.headers.keys();
+      const headers = keys.map((key) => `${key}: ${res.headers.get(key)}`);
+      console.log(res.body, headers);
+    });
   }
 
 }
